5f9c51772868e7e7914abacaf0bd09c7a613ee26
Anderson Brasil <andersonbrasil72@gmail.com>
2025-09-24 16:55:43 -0300

    Push

---
+++ public/build/assets/app-layout-BSshuS44.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-CR3eLBj1.js";import{A as s}from"./app-sidebar-layout-CYffyFM9.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
---
+++ public/build/assets/edit-branch-C16t5LLw.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-CR3eLBj1.js";function e(){return t.jsx("div",{children:"EditTenant"})}export{e as default};
