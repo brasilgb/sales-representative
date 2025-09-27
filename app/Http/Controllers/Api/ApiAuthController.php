dafc4441cc367175ee5fc1796061ddae2ab94fb6
Anderson Brasil <andersonbrasil72@gmail.com>
2025-09-27 17:24:00 -0300

    Push

--- app/Http/Controllers/Api/ApiAuthController.php
+++ app/Http/Controllers/Api/ApiAuthController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\User;
 use App\Models\Tenant;
-use App\Http\Controllers\Api\ApiBaseController as BaseController
+use App\Http\Controllers\Api\ApiBaseController as BaseController;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
