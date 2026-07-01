93a8fe64510b5234d2e0659d08c34eac391d771c
Anderson Brasil <andersonbrasil72@gmail.com>
2026-07-01 14:48:31 -0300

    Push

--- public/build/assets/app-alert-error-DJCuj6JA.js
+++
@@ -1,6 +0,0 @@
-import{j as e}from"./app-Dt6rCNwC.js";import{A as t,a as c}from"./alert-D-EUTWui.js";import{c as i}from"./createLucideIcon-Be19DJa0.js";/**
- * @license lucide-react v0.475.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const l=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["line",{x1:"12",x2:"12",y1:"8",y2:"12",key:"1pkeuh"}],["line",{x1:"12",x2:"12.01",y1:"16",y2:"16",key:"4dfq90"}]],a=i("CircleAlert",l);function m({message:r,className:s}){return e.jsx("div",{className:`p-4 ${s}`,children:e.jsxs(t,{className:"border-red-200 bg-red-50 text-red-600 relative",children:[e.jsx(a,{className:"h-4 w-4"}),e.jsx(c,{className:"flex items-center justify-between",children:e.jsx("span",{className:"text-red-800",children:r})})]})})}export{m as A};
--- public/build/assets/edit-branch-CWfrhRiv.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-Dt6rCNwC.js";/* empty css            */function r(){return t.jsx("div",{children:"EditTenant"})}export{r as default};
